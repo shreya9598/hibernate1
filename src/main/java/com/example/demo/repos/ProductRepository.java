6910e933c0745f728d646d8e4ae1a86239c92976
shreya9598 <shreyaagupta0511@gmail.com>
2023-02-07 11:31:07 +0530

    initial commit

--- src/main/java/com/example/demo/repos/ProductRepository.java
+++ src/main/java/com/example/demo/repos/ProductRepository.java
@@ -2,27 +2,42 @@ package com.example.demo.repos;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entities.Product;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.example.demo.entities.Product;
 
-//public interface ProductRepository extends CrudRepository<Product, Integer>{
-//
-//    List<Product> findByName(String name);
-//    List<Product> findByNameAndDesc(String name, String desc);
-//    List<Product> findByPriceGreaterThan(String price);
-//
-//
-//
-//}
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer>{
+    
     List<Product> findByName(String name);
-    List<Product> findByNameAndDesc(String name, String desc);
-    List<Product> findByPriceGreaterThan(String price);
+     List<Product> findByNameAndDesc(String name, String desc);
+     List<Product> findByPriceGreaterThan(Double price);
+     List<Product> findByDescContains(String desc);
+     List<Product> findByPriceBetween(Double price1, Double price2);
+     List<Product> findByDescLike(String desc);
+     List<Product> findByIdIn(List<Integer> ids, Pageable pageable);
 
 
-
+     void save(Product product);
 }
+
+
+
+
+
+
+
+
+// public interface ProductRepository extends CrudRepository<Product, Integer>{
+    
+//     List<Product> findByName(String name);
+//     List<Product> findByNameAndDesc(String name, String desc);
+//     List<Product> findByPriceGreaterThan(Double price);
+//     List<Product> findByDescContains(String desc);
+//     List<Product> findByPriceBetween(Double price1, Double price2);
+//     List<Product> findByDescLike(String desc);
+//     List<Product> findByIdIn(List<Integer> ids);
+
+
+// }
